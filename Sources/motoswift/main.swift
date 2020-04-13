86b5b919150ff5a8976769e2bccd7bf03507d699
Igor Palaguta <igor.palaguta@gmail.com>
2020-04-13 12:32:58 +0300

    use ArgumentParser instead of Commander

--- Sources/motoswift/main.swift
+++ Sources/motoswift/main.swift
@@ -1,29 +1,16 @@
-import Commander
+import ArgumentParser
 import Foundation
 
-private final class Application {
-    private let version = "0.5.0"
-
-    func run() {
-        let main = Group {
-            $0.addCommand("model",
-                          "generate code for your model",
-                          ModelCommand.command)
-            $0.addCommand("human",
-                          "generate human code for your model. Does not write to file, if file already exists",
-                          EntityCommand.human)
-            $0.addCommand("machine",
-                          "generate machine code for your model. Overwrites file every time",
-                          EntityCommand.machine)
-            $0.addCommand("version",
-                          "current version of MotoSwift",
-                          command { print(self.version) })
-        }
-
-        main.run()
-    }
+struct MotoSwift: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "Generates Swift NSManagedObject subclasses.",
+        subcommands: [
+            VersionCommand.self,
+            ModelCommand.self,
+            EntityCommand<HumanFile>.self,
+            EntityCommand<MachineFile>.self,
+        ]
+    )
 }
 
-private let application = Application()
-
-application.run()
+MotoSwift.main()
